80cabce786a2e1f82d0fda228654ea7b112d21c2
Jaimin.Prajapati <jaimin.prajapati@dakshyafreondtech.com>
2023-08-09 09:38:15 +0530

    Added ignoreUnknown true in MergeFields

--- src/main/java/io/github/dft/mailchimp/model/members/MergeFields.java
+++ src/main/java/io/github/dft/mailchimp/model/members/MergeFields.java
@@ -1,10 +1,12 @@
 package io.github.dft.mailchimp.model.members;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.UpperSnakeCaseStrategy.class)
 public class MergeFields {
 
